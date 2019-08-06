d504fb4d9b21c8d78e0575c52176164189514de1
Daniel Vu <daniel.vu@cerner.com>
2019-08-06 10:14:34 -0500

    Add initial structure

---
+++ server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const PORT = 4000 || process.env.PORT;
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Application listening on port ${PORT}`);
+});
